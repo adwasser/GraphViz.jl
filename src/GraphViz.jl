c5fd0079f51f2efc4af3242635e42651253962cd
Asher Wasserman <asher.d.wasserman@gmail.com>
2018-11-25 19:54:43 -0800

    end block

--- src/GraphViz.jl
+++ src/GraphViz.jl
@@ -592,69 +592,69 @@ if has_cairo
         end
         show(io, m, cairo_render(x))
     end
-    #=
-    if isdir(Pkg.dir("Gtk"))
-        using Gtk
-
-        function gtk_initialize(firstjob::Ptr{Nothing})
-            firstjob = convert(Ptr{GVJ_s},firstjob)
-            job = unsafe_load(firstjob)
-            c = unsafe_pointer_to_objref(job.context)::Gtk.Canvas
-            c.data = firstjob
-            job.context = getgc(c).ptr
-            job.window = pointer_from_objref(c)
-            unsafe_store!(firstjob,job)
-            nothing
-        end
-
-        function gtk_finalize(firstjob::Ptr{Nothing})
-            firstjob = convert(Ptr{GVJ_s},firstjob)
-            job = unsafe_load(firstjob)
-            c = unsafe_pointer_to_objref(job.window)::Gtk.Canvas
-            draw(gtk_update,c)
-            wait() #TODO: Make conditional on closing the widget
-            nothing
-        end 
-
-        function gtk_update(c::Canvas)
-            jobp = c.data::Ptr{GVJ_s}
-            job = unsafe_load(jobp)
-            job.height              = height(c)
-            job.width               = width(c)
-            job.context             = getgc(c).ptr
-            job.external_context    = 0x1
-            unsafe_store!(jobp,job)
-            println(job.callbacks)
-            println(unsafe_load(job.callbacks).refresh)
-            ccall(unsafe_load(job.callbacks).refresh,Nothing,(Ptr{Nothing},),jobp)
-        end
-        const gtk_engine = [ gvdevice_engine_t(@cfunction(gtk_initialize,Nothing,(Ptr{Nothing},)),C_NULL,@cfunction(gtk_finalize,Nothing,(Ptr{Nothing},))) ]
-        const gtk_features = [ gvdevice_features_t(Int32(GVDEVICE_EVENTS),0.,0.,0.,0.,96.,96.) ]
-        const gtk_name = Vector{UInt8}("julia_gtk:cairo")
-        const gtk_libname = Vector{UInt8}("julia_gtk:cairo")
-        const gtk_device = 
-            [ 
-                gvplugin_installed_t(Int32(0),pointer(gtk_name), Int32(0), pointer(gtk_engine), pointer(gtk_features));
-                null(gvplugin_installed_t)
-            ]
-        const gtk_api = 
-            [
-                gvplugin_api_t(API_device, pointer(gtk_device))
-                null(gvplugin_api_t)
-            ]
-
-        add_julia_gtk!(c::Context) = ccall((:gvAddLibrary,gvc),Nothing,(Ptr{Nothing},Ptr{gvplugin_library_t}),c.handle,[gvplugin_library_t(pointer(gtk_libname),pointer(gtk_api))])
-
-        function render(c::Gtk.Canvas,cg::Context,g::Graph) 
-            @async begin
-                add_julia_gtk!(cg)
-                ccall((:gvRenderContext,gvc),Cint,(Ptr{Nothing},Ptr{Nothing},Ptr{UInt8},Any),cg.handle,g.handle,"julia_gtk",c)    
-            end
-            nothing
-        end
-    end
-    =#
 end
+#=
+if isdir(Pkg.dir("Gtk"))
+    using Gtk
+
+    function gtk_initialize(firstjob::Ptr{Nothing})
+        firstjob = convert(Ptr{GVJ_s},firstjob)
+        job = unsafe_load(firstjob)
+        c = unsafe_pointer_to_objref(job.context)::Gtk.Canvas
+        c.data = firstjob
+        job.context = getgc(c).ptr
+        job.window = pointer_from_objref(c)
+        unsafe_store!(firstjob,job)
+        nothing
+    end
+
+    function gtk_finalize(firstjob::Ptr{Nothing})
+        firstjob = convert(Ptr{GVJ_s},firstjob)
+        job = unsafe_load(firstjob)
+        c = unsafe_pointer_to_objref(job.window)::Gtk.Canvas
+        draw(gtk_update,c)
+        wait() #TODO: Make conditional on closing the widget
+        nothing
+    end 
+
+    function gtk_update(c::Canvas)
+        jobp = c.data::Ptr{GVJ_s}
+        job = unsafe_load(jobp)
+        job.height              = height(c)
+        job.width               = width(c)
+        job.context             = getgc(c).ptr
+        job.external_context    = 0x1
+        unsafe_store!(jobp,job)
+        println(job.callbacks)
+        println(unsafe_load(job.callbacks).refresh)
+        ccall(unsafe_load(job.callbacks).refresh,Nothing,(Ptr{Nothing},),jobp)
+    end
+    const gtk_engine = [ gvdevice_engine_t(@cfunction(gtk_initialize,Nothing,(Ptr{Nothing},)),C_NULL,@cfunction(gtk_finalize,Nothing,(Ptr{Nothing},))) ]
+    const gtk_features = [ gvdevice_features_t(Int32(GVDEVICE_EVENTS),0.,0.,0.,0.,96.,96.) ]
+    const gtk_name = Vector{UInt8}("julia_gtk:cairo")
+    const gtk_libname = Vector{UInt8}("julia_gtk:cairo")
+    const gtk_device = 
+        [ 
+            gvplugin_installed_t(Int32(0),pointer(gtk_name), Int32(0), pointer(gtk_engine), pointer(gtk_features));
+            null(gvplugin_installed_t)
+        ]
+    const gtk_api = 
+        [
+            gvplugin_api_t(API_device, pointer(gtk_device))
+            null(gvplugin_api_t)
+        ]
+
+    add_julia_gtk!(c::Context) = ccall((:gvAddLibrary,gvc),Nothing,(Ptr{Nothing},Ptr{gvplugin_library_t}),c.handle,[gvplugin_library_t(pointer(gtk_libname),pointer(gtk_api))])
+
+    function render(c::Gtk.Canvas,cg::Context,g::Graph) 
+        @async begin
+            add_julia_gtk!(cg)
+            ccall((:gvRenderContext,gvc),Cint,(Ptr{Nothing},Ptr{Nothing},Ptr{UInt8},Any),cg.handle,g.handle,"julia_gtk",c)    
+        end
+        nothing
+    end
+end
+=#
 
 graph_plugins(c::Context) = Graph(ccall((:gvPluginsGraph,gvc),Ptr{Nothing},(Ptr{Nothing},),c.handle))
 
@@ -672,4 +672,5 @@ function listPlugins(c,kind)
     c_free(r)
     ret
 end
+
 end
